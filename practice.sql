33b92b8a052d95141a038878164c75f97a167de5
himanshu-r <himanshureddy3@gmail.com>
2021-08-31 22:57:43 +0530

    Created Postgresql Select Into

--- practice.sql
+++ practice.sql
@@ -10,3 +10,18 @@ CREATE TABLE acounts (
     last_login TIMESTAMP 
 
 );
+
+-- Select INTO
+
+SELECT
+    film_id,
+    title,
+    rental_rate
+INTO TABLE film_r
+FROM
+    film
+WHERE
+    rating = 'R'
+AND rental_duration = 5
+ORDER BY
+    title;
